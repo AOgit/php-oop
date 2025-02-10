16d5b45dc5ff556cef4661fc5637101983215b55
Oleksandr <git.hub@ukr.net>
2025-02-10 21:32:47 +0100

    done! Урок 11.Интерфейсы и контроль типа

---
+++ 11/index.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'classes/I3D.php';
+require_once 'classes/IGadget.php';
+require_once 'classes/Product.php';
+require_once 'classes/BookProduct.php';
+require_once 'classes/NotebookProduct.php';
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, true) . '</pre>';
+}
+
+function offerCase(IGadget $product){
+    echo "<p>Предлагаем чехол для гаджета {$product->getName()}<br>";
+}
+
+$book = new BookProduct('Три мушкетера', 20, 400);
+$notebook = new NotebookProduct('Dell', 1000, 'Intel');
+
+var_dump($notebook instanceof IGadget);
+offerCase($notebook);
+// offerCase($book);
+
+debug($book);
+
+echo $book->getProduct();
+
+
+class A {};
+class B extends A {};
+class C{};
+
+$a = new A();
+$b = new B();
+$c = new C();
+
+var_dump($a instanceof A);
+echo "<br>";
+var_dump($b instanceof A);
+echo "<br>";
+var_dump($c instanceof c);
