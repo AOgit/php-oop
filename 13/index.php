efa1a11e03a0d30126a8d11ac37ef82aa0905f11
Oleksandr <git.hub@ukr.net>
2025-02-11 19:54:29 +0100

    done! Урок 13.Composer и автозагрузка

---
+++ 13/index.php
@@ -0,0 +1,45 @@
+<?php
+
+error_reporting(-1);
+
+use wfm\interfaces\{I3, IGadget};
+use wfm\Product;
+use app\{BookProduct, NotebookProduct};
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// function autoloader($class)
+// {
+//     // echo 1 . $class . "<br>";
+//     // $file = __DIR__ . DIRECTORY_SEPARATOR . "classes". DIRECTORY_SEPARATOR . "{$class}.php";
+//     $file = __DIR__ . DIRECTORY_SEPARATOR . "{$class}.php";
+//     echo $file . "<br>";
+//     if (file_exists($file)){
+//         require_once $file;
+//     }
+// }
+
+// spl_autoload_register('autoloader');
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, true) . '</pre>';
+}
+
+function offerCase(IGadget $product){
+    echo "<p>Предлагаем чехол для гаджета {$product->getName()}<br>";
+}
+
+$book = new BookProduct('Три мушкетера', 20, 400);
+$notebook = new NotebookProduct('Dell', 1000, 'Intel');
+
+// var_dump($notebook instanceof IGadget);
+offerCase($notebook);
+// offerCase($book);
+
+debug($book);
+
+echo $book->getProduct();
+
+$mail = new \PHPMailer\PHPMailer\PHPMailer();
+debug($mail);
